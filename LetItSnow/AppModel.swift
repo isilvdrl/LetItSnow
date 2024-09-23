48901e9035af09d5b27a064d9bfe76c7f97efb96
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-09-23 19:58:39 +0300

    Initial Commit

---
+++ LetItSnow/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  LetItSnow
+//
+//  Created by IŞIL VARDARLI on 23.09.2024.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ LetItSnow/LetItSnowApp.swift
@@ -0,0 +1,33 @@
+//
+//  LetItSnowApp.swift
+//  LetItSnow
+//
+//  Created by IŞIL VARDARLI on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LetItSnowApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
